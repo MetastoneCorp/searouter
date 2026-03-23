80922e1de5f6362475d481e130bd079dc19bccba
xuweiming <xuweiming@metastonecorp.com>
2026-03-23 14:13:39 +0800

    fix: replace QuantumNous/new-api with searouter/searouter in logger

--- logger/logger.go
+++ logger/logger.go
@@ -10,8 +10,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/QuantumNous/new-api/common"
-	"github.com/QuantumNous/new-api/setting/operation_setting"
+	"github.com/searouter/searouter/common"
+	"github.com/searouter/searouter/setting/operation_setting"
 
 	"github.com/bytedance/gopkg/util/gopool"
 	"github.com/gin-gonic/gin"
